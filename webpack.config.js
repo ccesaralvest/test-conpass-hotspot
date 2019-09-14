661d2bebaebf7d9217323e965745a087fb4341a6
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-09-14 05:03:52 -0300

    Project Structure - Image Loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,19 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      bypassOnDebug: true,
+                      disable: true,
+                    },
+                  },
+                ],
+            },
+            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
