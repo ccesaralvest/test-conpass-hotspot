f0020a1490b7249a1881f2c9aece7a5701374d99
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-09-14 17:24:38 -0300

    Test Hotspot Widget

--- src/components/hotspotWidget/hotspotWidget.spec.js
+++ src/components/hotspotWidget/hotspotWidget.spec.js
@@ -1,13 +1,24 @@
 import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 import { shallow, configure } from 'enzyme';
 import React from 'react';
 import HotspotWidget from './index';
+import findByTestAtrr from './../../utils.js'
 
 describe('HotspotWidget component', () => {
+    const setUp = (props = {}) => {
+        const component = shallow(<HotspotWidget {...props} />);
+        return component;
+    };
+    let component;
+    
+    beforeEach(() => {
+        component = setUp();
+    });
+
     it('Should be render', () => {
-        const wrapper = shallow(<HotspotWidget />);
-        expect(wrapper.find('.hotspot').length).toBe(1);
-    })
+        const wrapper = findByTestAtrr(component,'hotspotWidget' );
+        expect(wrapper.length).toBe(1);
+    });
 });
