d774e27fbc722427df5c25b0b26a23d0b5734169
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-09-14 03:54:34 -0300

    Components Tests

--- src/components/header/header.spec.js
+++ src/components/header/header.spec.js
@@ -1,13 +1,36 @@
 import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 import { shallow, configure } from 'enzyme';
 import React from 'react';
 import Header from './index';
+import findByTestAtrr from './../../utils.js'
+
 
 describe('Header component', () => {
-    it('Should be render', () => {
-        const wrapper = shallow(<Header />);
-        expect(wrapper.find('header').length).toBe(1);
+    const setUp = (props = {}) => {
+        const component = shallow(<Header {...props} />);
+        return component;
+    };
+
+    let component;
+    
+    beforeEach(() => {
+        component = setUp();
     })
+
+    it('Should be render', () => {
+        const wrapper = findByTestAtrr(component,'header' );
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('Should has a Logo', () => {
+        const wrapper = findByTestAtrr(component,'logo-conpass' );
+        expect(wrapper.length).toBe(1);
+    });
+
+    it('Should has 4 options', () => {
+        const wrapper = findByTestAtrr(component,'option-item' );
+        expect(wrapper.length).toBe(4);
+    });
 });
